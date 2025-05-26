f92050e3a75b0ef7716326157d22f62cf2da6088
Rajendra karki <rajendra@uxcam.com>
2025-05-26 18:52:06 +0545

    updated repo path to beta

--- Package.swift
+++ Package.swift
@@ -49,7 +49,7 @@ let package = Package(
 
         .binaryTarget(
             name: "UXCam",
-            url: "https://raw.githubusercontent.com/uxcam/\(repoName)/beta/\(version)/UXCam.xcframework.zip",
+            url: "https://raw.githubusercontent.com/uxcam/\(repoName)/refs/heads/beta/UXCam.xcframework.zip",
             checksum: checksum
         )
     ]
